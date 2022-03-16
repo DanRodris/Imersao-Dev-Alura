9d9350201f79d18907e866d540c11149ff44d711
Danilo Rodrigues <84294007+DanRodris@users.noreply.github.com>
2022-03-16 14:41:31 -0300

    Update app.js

--- Aula08/app.js
+++ Aula08/app.js
@@ -1,5 +1,6 @@
 var carta1 = {
     nome: "Bulbassauro",
+    Imagem:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.G9gx0bfTaObAzeTUIYVpGgHaFM%26pid%3DApi&f=1",
     atributos:{
         Ataque: 7,
         Defesa: 8,
@@ -10,6 +11,7 @@ var carta1 = {
 
 var carta2 = {
     nome: "Darth Vader",
+    imagem:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.eu1eBrrow3_BYyEZ1wSCywHaJA%26pid%3DApi&f=1",
     atributos:{
         Ataque: 8,
         Defesa: 9,
@@ -20,6 +22,7 @@ var carta2 = {
 
 var carta3 = {
     nome: "Shiryu do Dragão",
+    Imagem: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F7a%2F43%2F87%2F7a438762e112faf390c0ca8a31006df1.jpg&f=1&nofb=1",
     atributos:{
         Ataque: 10,
         Defesa: 8,
@@ -51,20 +54,12 @@ function sortearCarta() {
     document.getElementById("btnSortear").disabled = true;
     document.getElementById("btnJogar").disabled = false;
 
-    exibirOpcoes()
+    
+    exibirCartaJogador()
 }
 
 
 
-function exibirOpcoes(){
-    var opcoes = document.getElementById("opcoes");
-    var opcoesTexto = ""
-
-    for ( var atributo in cartaJogador.atributos){
-        opcoesTexto += "<input type = 'radio' name ='atributo' value='" + atributo + "'>" + atributo
-    }
-    opcoes.innerHTML = opcoesTexto;
-}
 
 function obtemAtributoSelecionado(){
     var radioAtributos = document.getElementsByName("atributo");
@@ -95,3 +90,21 @@ function jogar(){
     console.log(cartaJogador)
 
 }
+
+function exibirCartaJogador(){
+    var divCartaJogador = document.getElementById("carta-jogado");
+    divCartaJogador.style.backgroundImage ='url(${cartaJogador.imagem})';
+    //divCartaJogador.style.backgroundImage ="url(" + cartaJogador.imagem +")"
+
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021 card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+     var tagHtml = "<div id='opcoes' class=' cartas-status'>"
+    
+    var opcoesTexto = "";
+    for ( var atributo in cartaJogador.atributos){
+        opcoesTexto += "<input type = 'radio' name ='atributo' value='" + atributo + "'>" + atributo + "" + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var nome = '<p class="carta-subtitle">${cartaJogador.nome}</P>'
+    divCartaJogador.innerHTML = moldura + nome + tagHtml + opcoesTexto + '</div>'
+
+    }
